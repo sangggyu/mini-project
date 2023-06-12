09820fa34fb4d76e63bd3e01c0e3d8f746d45bf4
sangggyu <limsanggyu@kakao.com>
2023-06-12 12:34:58 +0900

    feature : project 생성

---
+++ mini-project-board/src/main/java/com/example/miniprojectboard/MiniProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectBoardApplication.class, args);
+    }
+
+}
