133bfa8c8a6fefcbb0ae7132a90974f43436a9da
sangggyu <limsanggyu@kakao.com>
2023-06-22 15:54:13 +0900

    #14 - 게시글 관련 뷰 테스트 작성 어떤 경로로 뷰를 열어줄지 테스트로 명세를 받는다.

---
+++ mini-project-board/src/main/java/com/example/miniprojectboard/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.example.miniprojectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
