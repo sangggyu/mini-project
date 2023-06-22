cd5611a05f11e783d2acb71840193419daec576d
sangggyu <limsanggyu@kakao.com>
2023-06-22 17:16:18 +0900

    #14 - 게시판 뷰 기본 골격 작성

--- mini-project-board/src/test/java/com/example/miniprojectboard/controller/ArticleControllerTest.java
+++ mini-project-board/src/test/java/com/example/miniprojectboard/controller/ArticleControllerTest.java
@@ -21,7 +21,7 @@ class ArticleControllerTest {
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
-    @Disabled("구현 중")
+
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -30,9 +30,9 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
-                .andExpect(view().name("articles"));
+                .andExpect(model().attributeExists("articles"));
 
     }
     @Disabled("구현 중")
