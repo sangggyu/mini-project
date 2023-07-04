98b15c7c40557b876eea8616e2dd495ed5ae857b
sangggyu <limsanggyu@kakao.com>
2023-07-04 16:31:02 +0900

    #24 - 해시태그 검색 페이지를 위한 기능 구현

--- mini-project-board/src/main/java/com/example/miniprojectboard/service/ArticleService.java
+++ mini-project-board/src/main/java/com/example/miniprojectboard/service/ArticleService.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @Transactional
@@ -62,4 +64,19 @@ public class ArticleService {
         articleRepository.deleteById(articleId);
     }
 
+    public Long getArticleCount() {
+        return articleRepository.count();
+    }
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
+        if (hashtag == null || hashtag.isBlank()) {
+            return Page.empty(pageable);
+        }
+
+        return articleRepository.findByHashtag(hashtag, pageable).map(ArticleDto::from);
+    }
+
+    public List<String> getHashtags() {
+        return articleRepository.findAllDistincHashtag();
+    }
 }
