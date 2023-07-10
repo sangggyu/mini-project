c6cd73e02badee8d705e49869ffa51eee07c22af
sangggyu <limsanggyu@kakao.com>
2023-07-10 16:20:20 +0900

    오류해결

---
+++ mini-project-admin/src/test/java/com/example/miniprojectadmin/MiniProjectAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniprojectadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
